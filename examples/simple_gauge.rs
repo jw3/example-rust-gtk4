3b50f2e2f208ded2690e37155084438808d2af7f
John Wass <jwass3@gmail.com>
2022-08-27 07:21:35 -0400

    move to ApplicationWindow

--- examples/simple_gauge.rs
+++ examples/simple_gauge.rs
@@ -1,6 +1,6 @@
 use std::thread::{sleep, Thread};
 use std::time::Duration;
-use gtk::{Builder, Window};
+use gtk::{Builder, ApplicationWindow};
 use gtk::prelude::*;
 
 use example_gtk4::simple_gauge::SimpleGauge;
@@ -16,7 +16,7 @@ fn main() {
     application.connect_activate(|app| {
         let glade_src = include_str!("../src/gauge.glade");
         let builder = Builder::from_string(glade_src);
-        let window: Window = builder.object("main_window").unwrap();
+        let window: ApplicationWindow = builder.object("main_window").unwrap();
         window.set_application(Some(app));
         window.present();
     });
