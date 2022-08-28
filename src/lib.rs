10d8612456d9a1573e3e68c2fc94df0f110ca97d
John Wass <jwass3@gmail.com>
2022-08-28 07:23:34 -0400

    attempt at glade catalog

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
 pub mod simple_gauge;
+
+#[no_mangle]
+pub extern "C" fn gauges_catalog_init() {
+    // SimpleGauge::ensure_type();
+}
+
+// #[no_mangle]
+// pub extern "C" fn simple_gauge_get_type() -> GType {
+//     SimpleGauge::ensure_type();
+//     SimpleGauge::static_type().into_glib()
+// }
